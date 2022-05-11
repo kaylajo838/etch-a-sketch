dfe085745bd2d4e179bb6fd68e3a02a59f219e7c
kaylajo838 <kaylaimming@gmail.com>
2022-05-11 12:38:14 -0700

    add functions to buttons sliders and to create grid based on slider values

--- javascript.js
+++ javascript.js
@@ -1,22 +1,60 @@
+const grid = document.getElementById('grid');
+const resetBtn = document.getElementById('reset');
+const slider = document.getElementById('sizeSlider');
+let gridValue = document.getElementById('slider-value');
+let gridSize = document.querySelector('input');
+const applyGridSize = document.getElementById('apply');
+const container = document.getElementById('container');
 
 
-
-function id(id) {
-    return document.getElementById(id);
+//function to get slider value
+function showValue(x) {
+    document.getElementById('slider-value').textContent = `Grid Size: ${x} x ${x}`;
 }
+gridSize.addEventListener('input', function (e) {
+    squareSize = e.target.value;
+    gridValue.textContent = `Grid Size: ${squareSize}x${squareSize}`;
+});
 
-let count = 0;
-let idCount = 0;
-
-window.onload = () => {
-    for (let i = 0; i < 81; i++) {
-        // Create tile & give it CSS of the tile then append it
-        let tile = document.createElement('p');
-        tile.id = idCount;
-        idCount++
-        tile.classList.add('tile');
-        tile.textContent = '';
-
-        id('grid').appendChild(tile);
+// function to create grid inside initBox
+function createGrid(col, rows) {
+    for (let i = 0; i < (col * rows); i++) {
+        const div = document.createElement('div');
+        grid.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        grid.appendChild(div).classList.add('box');
     }
 }
+
+// modify grid size 
+function modifyGridSize() {
+    let boxes = grid.querySelectorAll(".box");
+    boxes.forEach(box => box.remove());
+    createGrid(slider.value, slider.value);
+}
+
+// make 16x16 grid when window loads
+window.onload = createGrid(slider.value, slider.value);
+
+// change grid size with slider and apply button
+applyGridSize.addEventListener('click', modifyGridSize);
+
+
+// reset grid to 16x16 when reset button is clicked
+resetBtn.addEventListener('click', function () {
+    let boxes = grid.querySelectorAll(".box");
+    boxes.forEach(box => box.remove());
+    createGrid(16, 16);
+    slider.value = 16;
+    gridValue.textContent = `Grid Size: 16 x 16`;
+})
+
+
+// function to toggle lines on and off with button
+const toggleBtn = document.getElementById('toggle');
+
+toggleBtn.addEventListener('click', () => {
+    let boxes = grid.querySelectorAll('.box');
+    boxes.forEach(box => box.classList.toggle('toggle-lines'));
+})
+
